efb02229533a04dbada0adc60dc7df33bcc9865f
rojinhood <deneme9167@gmail.com>
2024-03-04 19:33:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function correlation(x, y) {
+    // Calculate the mean of the two arrays
+    let x̄ = x.reduce((a, b) => a + b) / x.length;
+    let ȳ = y.reduce((a, b) => a + b) / y.length;
+  
+    // Calculate the correlation coefficient
+    let r = 0;
+    for (let i = 0; i < x.length; i++) {
+      r += (x[i] - x̄) * (y[i] - ȳ);
+    }
+    r /= Math.sqrt(x.map(xi => (xi - x̄) ** 2).reduce((a, b) => a + b)) * Math.sqrt(y.map(yi => (yi - ȳ) ** 2).reduce((a, b) => a + b));
+  
+    return r;
+  }
+  
+
